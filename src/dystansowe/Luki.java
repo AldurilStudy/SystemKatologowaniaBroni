be03c7f38725c25b95636e56d2a02833d25e1c50
Piotrek1900 <pbienkowski1990@gmail.com>
2015-01-22 09:17:06 +0100

    dodanie broni dystansowej

---
+++ src/dystansowe/Luki.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dystansowe;
+
+/**
+ *Klasa służy do reprezentowania Łuków z broni dystansowej
+ * @Piotr Bieńkowski
+ */
+public class Luki {
+    private String typ;
+    private String nazwa;
+    private String pochodzenie;
+    
+    private int wiek;
+
+public Luki () {
+    
+    System.out.print("Utworzoną nową broń dystansową  Łuk");
+}
+}
