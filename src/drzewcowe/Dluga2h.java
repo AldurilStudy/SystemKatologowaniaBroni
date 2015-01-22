61d71974e5bcfe500781df9e8942f92a92b1b4ba
AldurilStudy <skop.pawel1@gmail.com>
2015-01-22 08:47:00 +0100

    Początek projektu i bronie drzewcowe

---
+++ src/drzewcowe/Dluga2h.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drzewcowe;
+
+/**
+ *
+ * @author student
+ */
+public class Dluga2h {
+    private String typ;
+    private String nazwa;
+    private String pochodzenie;
+    
+    private int wiek;
+    
+    public Dluga2h(){
+        System.out.println("Utworzono nową broń drzewcową dwuręczną!");
+    }
+}
