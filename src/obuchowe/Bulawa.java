85ea170900f3b87deab7025eead3dfc881dd4c6d
Piotrek1900 <pbienkowski1990@gmail.com>
2015-01-22 09:06:03 +0100

    dodanie obuchowych

---
+++ src/obuchowe/Bulawa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obuchowe;
+
+/**
+ *Klasa służy do reprezentowania buław z broni obuchowych
+ * @Piotr Bieńkowski
+ */
+public class Bulawa {
+     private String typ;
+    private String nazwa;
+    private String pochodzenie;
+    
+    private int wiek;
+
+public Bulawa () {
+    
+    System.out.print("Utworzoną nową broń obuchową  buławę");
+}
+}
---
+++ src/obuchowe/Korbacz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obuchowe;
+
+/**
+ *Klasa służy do reprezentowania korbaczy z broni obuchowych
+ * @Piotr Bieńkowski
+ */
+public class Korbacz {
+    private String typ;
+    private String nazwa;
+    private String pochodzenie;
+    
+    private int wiek;
+    
+    public Korbacz(){
+        System.out.print("Utworzoną nową broń obuchową  korbacz");
+    }
+}
