b514813f64f51873fb6cfcfca65319c6370a3bd9
sowakin <wojtek.bolesta@wp.pl>
2015-01-22 09:15:22 +0100

    pomidor

---
+++ Desktop/SystemKatologowaniaBroni-master/SystemKatologowaniaBroni-master/src/sieczne/Miecz15h.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sieczne;
+
+/**
+ *
+ * @author student
+ */
+public class Miecz15h {
+    private String typ;
+    private String nazwa;
+    private String pochodzenie;
+    
+    private int wiek;
+    
+    public Miecz15h(){
+        System.out.println("Utworzono nową broń sieczną półtoraręczną!");
+}
+}
---
+++ Desktop/SystemKatologowaniaBroni-master/SystemKatologowaniaBroni-master/src/sieczne/Topor2h.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sieczne;
+
+/**
+ *
+ * @author student
+ */
+public class Topor2h {
+    private String typ;
+    private String nazwa;
+    private String pochodzenie;
+    
+    private int wiek;
+    
+    public Topor2h(){
+        System.out.println("Utworzono nową broń sieczną topór dwóręczny!");
+}
+}
