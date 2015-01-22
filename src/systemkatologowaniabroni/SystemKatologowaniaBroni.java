011f010f8ce197c6d7cc9ca21ea3c37d47a9a078
sowakin <wojtek.bolesta@wp.pl>
2015-01-22 09:54:42 +0100

    poprawione repo

--- src/systemkatologowaniabroni/SystemKatologowaniaBroni.java
+++ src/systemkatologowaniabroni/SystemKatologowaniaBroni.java
@@ -5,10 +5,14 @@
  */
 package systemkatologowaniabroni;
 
-/**
- *
- * @author student
- */
+import sieczne.Miecz15h;
+import sieczne.Miecz1h;
+import sieczne.Miecz2h;
+import sieczne.Noze;
+import sieczne.Topor1h;
+import sieczne.Topor2h;
+
+
 public class SystemKatologowaniaBroni {
 
     /**
@@ -17,5 +21,12 @@ public class SystemKatologowaniaBroni {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+   sieczne.Miecz15h miecz15h = new Miecz15h();
+   sieczne.Miecz1h miecz1h = new Miecz1h();
+   sieczne.Miecz2h miecz2h = new Miecz2h();
+   sieczne.Noze noze = new Noze();
+   sieczne.Topor1h topor1h = new Topor1h();
+   sieczne.Topor2h topor2h = new Topor2h();
     
 }
